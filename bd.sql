c89751809e442a919e2244bcdbe9e01b5abd86fd
Капустина Кристина <kristinakapk@gmail.com>
2022-08-26 22:54:09 +0300

    скрипт джейсон/мускл

--- bd.sql
+++ bd.sql
@@ -2,12 +2,11 @@ CREATE TABLE zapis
 (
     userId INT,
     id INT,
-    name varchar(20) not null,
     title varchar(20) not null,
     body varchar(20) not null
 );
 
-INSERT INTO `zapis` (userId, id, name, title, body) VALUE -- тест
+INSERT INTO `zapis` (userId, id, title, body) VALUE -- тест
     (3, 1, 'Kassy', 'title_is', 'test'),
     (4, 2, 'lili', 'title_is', 'test');
 
--- script.php
+++ script.php
@@ -1,1 +1,25 @@
 <?php
+
+declare(strict_types=1);
+
+//подключение к БД через PDO
+$dsn = 'mysql:host=mysql;dbname=db;charset=UTF8';
+$user = 'user';
+$password = 'password';
+
+$url = 'https://jsonplaceholder.typicode.com/posts';
+//read the json file contents
+$jsondata_stati= file_get_contents($url);
+
+//convert json object to php associative array
+$data = json_decode($jsondata_stati, true);
+
+//get the details
+$userId = $data['userId'];
+$id = $data['id'];
+$title = $data['title'];
+$body = $data['body'];
+
+//insert into mysql table
+$sql = "INSERT INTO zapis(userId, id, title, body)
+    VALUES('$userId', '$id', '$title', '$body')";
