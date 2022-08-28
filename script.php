281dfc696de42e15ac4665908ae536fa1aaf3fbf
Капустина Кристина <kristinakapk@gmail.com>
2022-08-28 14:42:19 +0300

    загрузка в бд  осуществляется успешно

--- script.php
+++ script.php
@@ -7,76 +7,77 @@ $username = "user";
 $password = "password";
 
 try {
-$connect = new PDO("mysql:host=$servername;dbname=db", $username, $password);
-$connect ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connect = new PDO("mysql:host=$servername;dbname=db", $username, $password);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
     $url = 'https://jsonplaceholder.typicode.com/posts';
 
 // Read the JSON file in PHP
-    $jsondata_stati= file_get_contents($url);
+    $jsondata_stati = file_get_contents($url);
 
 
 // Convert the JSON String into PHP Array
-
     $array = json_decode($jsondata_stati, true);
 
 
-
 // Extracting row by row
-
-
-
     foreach ($array as $row) {
         $userId = $row['userId'];
         $id = $row['id'];
         $title = $row['title'];
         $body = $row['body'];
 
-
 //insert values into mysql database
-        $sql="INSERT INTO `zapis`(`userId`, `id`, `title`, `body`)
-VALUES ('$userId', '$id', '$title', '$body')";
+        /*$sql="INSERT INTO `zapis`(`userId`, `id`, `title`, `body`)
+VALUES ('$userId', '$id', '$title', '$body')";*/
+        $sql = "INSERT INTO zapis VALUES
+                          ('" . $row["userId"] . "', '" . $row["id"] . "', '" . $row["title"] . "', '" . $row["body"] . "'); ";
+
+
+        /*foreach($array as $row) {
+
+            // Database query to insert data
+
+            // into database Make Multiple
+
+            // Insert Query
+
+            $query .=
+
+                "INSERT INTO zapis VALUES
+                              ('".$row["userId"]."', '".$row["id"]."', '".$row["title"]."', '".$row["body"]."'); ";
+            $table_data .= '
+
+                        <tr>
+
+                            <td>'.$row["userId"].'</td>
+
+                            <td>'.$row["id"].'</td>
+
+                            <td>'.$row["title"].'</td>
+
+                            <td>'.$row["body"].'</td>
+
+                        </tr>
+
+                        ';}
+        */
+        $stmt = $connect->prepare($sql);
+
+        $success = $stmt->execute();
+        if ($success) {
+            echo "Загружено записей и комментариев!";
+        } else {
+            echo "Что-то пошло не так...";
+        }
+        $db = null;
     }
-
-    /*foreach($array as $row) {
-
-        // Database query to insert data
-
-        // into database Make Multiple
-
-        // Insert Query
-
-        $query .=
-
-            "INSERT INTO zapis VALUES
-                          ('".$row["userId"]."', '".$row["id"]."', '".$row["title"]."', '".$row["body"]."'); ";
-        $table_data .= '
-
-                    <tr>
-
-                        <td>'.$row["userId"].'</td>
-
-                        <td>'.$row["id"].'</td>
-
-                        <td>'.$row["title"].'</td>
-
-                        <td>'.$row["body"].'</td>
-
-                    </tr>
-
-                    ';}
-    */
-    $stmt = $connect->prepare($sql);
-    $success = $stmt->execute();
-    if($success){
-        echo "Форма отправлена успешно!";
-    } else {
-        echo "Что-то пошло не так...";
-    }
-    $db = null;
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
 }
+catch
+    (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
 
 
 
